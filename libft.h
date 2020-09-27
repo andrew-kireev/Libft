8379a0e19c0156363d9a39d829b56befc9b3b06c
Andrew <a-kireev1989@mail.ru>
2020-09-27 16:08:41 +0300

    done ft_memset.c

---
+++ libft.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fgwyneth <fgwyneth@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/09/27 15:45:01 by fgwyneth          #+#    #+#             */
+/*   Updated: 2020/09/27 15:52:38 by fgwyneth         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <string.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+void	*ft_memset(void *b, int c, size_t len);
+
+#endif
