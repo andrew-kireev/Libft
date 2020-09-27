656f2c12717054f1b55a705f7194f8cc08b123bc
Andrew <a-kireev1989@mail.ru>
2020-09-27 16:13:59 +0300

    doing norm

--- ft_memset.c
+++ ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: fgwyneth <fgwyneth@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/27 15:59:24 by fgwyneth          #+#    #+#             */
-/*   Updated: 2020/09/27 16:05:55 by fgwyneth         ###   ########.fr       */
+/*   Updated: 2020/09/27 16:13:04 by fgwyneth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,15 @@
 
 void	*ft_memset(void *b, int c, size_t len)
 {
-    char    *ptr;
-    size_t     i;
+	char	*ptr;
+	size_t	i;
 
-    i = 0;
-    ptr = b;
-    while (i != len)
-    {
-        ptr[i] = c;
-        i++;
-    }
-    return ptr;
+	i = 0;
+	ptr = b;
+	while (i != len)
+	{
+		ptr[i] = c;
+		i++;
+	}
+	return (ptr);
 }
